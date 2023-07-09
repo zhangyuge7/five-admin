c4baba193868e6532008449c87a4f36de3a4da4c
zhangyuge <398698424@qq.com>
2023-07-09 16:39:17 +0800

    外链接组件调整

--- src/layout/main/LayoutIframe.vue
+++ src/layout/main/LayoutIframe.vue
@@ -1,17 +1,22 @@
 <script setup>
-import { ref, watch } from 'vue'
-import router from '@/router'
+import { ref, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 const linkSrc = ref('')
-
+const load = ref(true)
+function handleIframeLoad() {
+  load.value = false
+}
 // 侦听路由
-watch(router.currentRoute, (val) => {
-  linkSrc.value = val.meta?.link
+watchEffect(() => {
+  load.value = true
+  linkSrc.value = route.meta?.link
 }, { immediate: true })
 </script>
 
 <template>
-  <div class="w-full h-full">
-    <iframe :src="linkSrc" class="w-full h-full" />
+  <div v-loading="load" class="w-full h-full">
+    <iframe :src="linkSrc" class="w-full h-full" @load="handleIframeLoad" />
   </div>
 </template>
--- src/layout/main/LayoutLink.vue
+++ src/layout/main/LayoutLink.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { watch } from 'vue'
+import { watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
 import router from '@/router'
 
+const route = useRoute()
 // 打开外链地址
 function openLink(route) {
   if (route.meta?.link && !route.meta.isIframe)
     window.open(router.currentRoute.value.meta.link)
 }
 // 侦听路由
-watch(router.currentRoute, (val) => {
-  openLink(val)
+watchEffect(() => {
+  openLink(route)
 }, { immediate: true })
 </script>
 
 <template>
-  <div>外链已打开</div>
+  <div>连接已打开</div>
 </template>
