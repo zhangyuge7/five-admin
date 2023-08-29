196ec64a5d27ca455ac905ffac74836c951725b1
zhangyuge <398698424@qq.com>
2023-08-29 21:55:21 +0800

    简单的404页面

--- src/views/error-page/404.vue
+++ src/views/error-page/404.vue
@@ -1,4 +1,5 @@
 <script setup name="NotFound">
+import { HomeFilled, RefreshLeft } from '@element-plus/icons-vue'
 import router from '@/router'
 
 function toIndex() {
@@ -10,17 +11,24 @@ function doBack() {
 </script>
 
 <template>
-  <div>
-    <p>404</p>
-    <el-button type="primary" @click="toIndex">
-      回首页
-    </el-button>
-    <el-button type="primary" @click="doBack">
-      回上一页
-    </el-button>
+  <div class="fv-error-404">
+    <div style="text-align: center;">
+      <el-button type="primary" :icon="HomeFilled" @click="toIndex">
+        回首页
+      </el-button>
+      <el-button type="primary" :icon="RefreshLeft" @click="doBack">
+        回上一页
+      </el-button>
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.fv-error-404{
+  width: 100%;
+  height: 100%;
+  padding-top: 40%;
+  background: url('/src/assets/images/error/404.png') no-repeat;
+  background-position: 50% 20%;
+}
 </style>
