925c509a7f953d9328b620dba00c6744d496eef6
zhangyuge <398698424@qq.com>
2023-08-29 21:55:10 +0800

    大屏页面的热更新问题

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { onMounted, reactive } from 'vue'
 import useElementI18n from './hooks/useElementI18n'
 import { useAppStore } from '@/stores/modules/app'
 import { useRouteStore } from '@/stores/modules/route'
@@ -9,6 +10,18 @@ const appStore = useAppStore()
 const routeStore = useRouteStore()
 const useApp = useAppStore()
 const { locale } = useElementI18n()
+const state = reactive({
+  key: 0,
+})
+onMounted(() => {
+  // 监听开发期间的热更新，保证key唯一，解决 Transition 组件与 Component 组件在热更新时的 bug
+  if (import.meta.env.MODE === 'development' && appStore.appConfig.isTransition) {
+    import.meta.hot.on('vite:beforeUpdate', ({ type }) => {
+      if (type === 'update')
+        state.key++
+    })
+  }
+})
 </script>
 
 <template>
@@ -16,7 +29,7 @@ const { locale } = useElementI18n()
     <RouterView v-slot="{ Component, route }">
       <Transition v-if="appStore.appConfig.isTransition" :name="route.meta?.transition || appStore.appConfig.transitionName" mode="out-in">
         <KeepAlive :include="routeStore.keepAliveViews">
-          <Component :is="Component" />
+          <Component :is="Component" :key="state.key" />
         </KeepAlive>
       </Transition>
       <KeepAlive v-else :include="routeStore.keepAliveViews">
