096cea845dd64c4cdf63679fe760a9ed99d27e61
zhangyuge <398698424@qq.com>
2023-09-15 21:30:46 +0800

    i18n别名问题

--- vite/alias.js
+++ vite/alias.js
@@ -3,6 +3,6 @@ import { URL, fileURLToPath } from 'node:url'
 const alias = {
   '@': fileURLToPath(new URL('../src', import.meta.url)),
   // 'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
-  "vue-i18n": "vue-i18n/dist/vue-i18n.esm-bundler.js", // 修改后
+  'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js', // 修改后
 }
 export default alias
