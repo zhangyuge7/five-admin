b4d8f4d97241d65eb7469030290c3edec730f30d
zhangyuge <398698424@qq.com>
2023-09-25 19:57:34 +0800

    首页页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -25,15 +25,28 @@ Object.keys(packagejson.devDependencies).forEach((key) => {
     <el-main>
       <el-row :gutter="20">
         <el-col :span="12" :offset="0">
-          <el-card>
+          <el-card shadow="never">
             <template #header>
-              版本说明
+              简介
             </template>
-            1
+            <p>
+              项目基于 JavaScript、Vue3、Vite4、Pinia、ElementPlus、Tailwindcss 等技术栈开发。
+              为开发后台管理系统提供开箱即用的解决方案，使开发者可以轻松构建功能齐全的管理系统界面。
+              使用 FiveAdmin，开发者可以通过简单的配置，快速搭建后台管理系统，提高开发效率。
+            </p>
+
+            <p>
+              作者在写FiveAdmin之前市面上已经有大量的同品优秀开源项目，FiveAdmin 可能并不比它们优秀，但是现有的优秀项目中没有一款符合作者的“口味”。
+              本着打造一款符合自己的技术栈要求的目的写的这么个项目，同时也为了深入学习并熟悉前端的通用技术。
+            </p>
+
+            <p>
+              使用过程中如果有好的建议，欢迎指教。
+            </p>
           </el-card>
         </el-col>
         <el-col :span="12" :offset="0">
-          <el-card>
+          <el-card shadow="never">
             <template #header>
               <div>
                 <span>相关依赖</span>
