edee9bc300a8e1a738b75560d0a8749d8d593fd0
zhangyuge <398698424@qq.com>
2023-09-25 21:08:00 +0800

    大屏示例页面添加回首页

--- src/views/demo/data-big-screen/BigScreen1.vue
+++ src/views/demo/data-big-screen/BigScreen1.vue
@@ -2,15 +2,18 @@
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
 
-const count = ref(0)
+const val = ref()
 </script>
 
 <template>
   <div>
     数据大屏1-缓存组件
-    <el-input-number v-model="count" />
+    <el-input v-model="val" placeholder="请输入" style="width: 200px;" />
     <RouterLink to="/screen/2">
       去大屏2
+    </RouterLink>
+    <RouterLink to="/">
+      回首页
     </RouterLink>
   </div>
 </template>
--- src/views/demo/data-big-screen/BigScreen2.vue
+++ src/views/demo/data-big-screen/BigScreen2.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 
-const count = ref(0)
+const val = ref()
 </script>
 
 <template>
   <div>
     数据大屏2
-    <el-input-number v-model="count" />
+    <el-input v-model="val" placeholder="请输入" style="width: 200px;" />
+
     <RouterLink to="/screen/1">
       去大屏1
+    </RouterLink>
+    <RouterLink to="/">
+      回首页
     </RouterLink>
   </div>
 </template>
