1c531e05584dbb27d2fcaff5637cf4c192f4d22d
zhangyuge <398698424@qq.com>
2023-11-02 21:57:02 +0800

    修改

--- src/views/user-info/index.vue
+++ src/views/user-info/index.vue
@@ -18,60 +18,58 @@ const columns = [
 </script>
 
 <template>
-  <el-container>
-    <el-main>
-      <el-row :gutter="20">
-        <el-col :span="6">
-          <el-card shadow="never">
-            <template #header>
-              <div>
-                <span>基本信息</span>
-              </div>
-            </template>
+  <el-main>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="never">
+          <template #header>
             <div>
-              <p class=" flex items-center">
-                <el-avatar :size="50" src="https://tse1-mm.cn.bing.net/th/id/OIP-C.jRmS7Hl4Q6YTW-sRDVqY7AAAAA?w=197&h=197&c=7&r=0&o=5&pid=1.7" />
-                <span class=" ml-2">路飞</span>
-              </p>
-              <p class="mt-2">
-                <span>角色：</span>
-                <span>海贼</span>
-              </p>
-              <p class="mt-2">
-                <span>组织机构：</span>
-                <span>草帽海贼团</span>
-              </p>
-              <p class="mt-2">
-                <span>职务：</span>
-                <span>船长</span>
-              </p>
-              <p class="mt-2">
-                <span>个人简介：</span>
-                <span>海贼王是大海之中最自由的人</span>
-              </p>
+              <span>基本信息</span>
             </div>
-          </el-card>
-        </el-col>
-        <el-col :span="18" :offset="0">
-          <el-card shadow="never">
-            <template #header>
-              <div>
-                <span>登陆记录</span>
-              </div>
-            </template>
-            <el-table :data="tableData" border stripe>
-              <el-table-column type="index" width="50" />
-              <el-table-column
-                v-for="col in columns"
-                :key="col.id"
-                :prop="col.id"
-                :label="col.label"
-                :width="col.width"
-              />
-            </el-table>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-main>
-  </el-container>
+          </template>
+          <div>
+            <p class=" flex items-center">
+              <el-avatar :size="50" src="https://tse1-mm.cn.bing.net/th/id/OIP-C.jRmS7Hl4Q6YTW-sRDVqY7AAAAA?w=197&h=197&c=7&r=0&o=5&pid=1.7" />
+              <span class=" ml-2">路飞</span>
+            </p>
+            <p class="mt-2">
+              <span>角色：</span>
+              <span>海贼</span>
+            </p>
+            <p class="mt-2">
+              <span>组织机构：</span>
+              <span>草帽海贼团</span>
+            </p>
+            <p class="mt-2">
+              <span>职务：</span>
+              <span>船长</span>
+            </p>
+            <p class="mt-2">
+              <span>个人简介：</span>
+              <span>海贼王是大海之中最自由的人</span>
+            </p>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="18" :offset="0">
+        <el-card shadow="never">
+          <template #header>
+            <div>
+              <span>登陆记录</span>
+            </div>
+          </template>
+          <el-table :data="tableData" border stripe>
+            <el-table-column type="index" width="50" />
+            <el-table-column
+              v-for="col in columns"
+              :key="col.id"
+              :prop="col.id"
+              :label="col.label"
+              :width="col.width"
+            />
+          </el-table>
+        </el-card>
+      </el-col>
+    </el-row>
+  </el-main>
 </template>
