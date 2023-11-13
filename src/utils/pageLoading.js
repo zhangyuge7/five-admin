2ff70d37d5088cb014bc600da19545695b92a17a
zhangyuge <398698424@qq.com>
2023-11-13 20:37:25 +0800

    主页面路由切换loading延时显示

--- src/utils/pageLoading.js
+++ src/utils/pageLoading.js
@@ -1,14 +1,23 @@
 import { ElLoading } from 'element-plus'
 
 let loadingInstance
+const timerArr = []
 export default {
-  start(el) {
-    loadingInstance = ElLoading.service({
-      target: el || document.querySelector('.fv-el-main'),
-      text: 'loading....',
-    })
+  start(delay = 350, target = document.querySelector('.fv-el-main')) {
+    const timer = setTimeout(() => {
+      loadingInstance = ElLoading.service({
+        target,
+        text: 'loading....',
+      })
+    }, delay)
+    timerArr.push(timer)
   },
   done() {
+    const length = timerArr.length
+    for (let i = 0; i < length; i++) {
+      const o = timerArr.shift()
+      o && clearTimeout(o)
+    }
     loadingInstance && loadingInstance.close()
   },
 }
